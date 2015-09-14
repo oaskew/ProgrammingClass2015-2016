b5a9420964b1747794534fe585ef03e52085bada
Omar Askew <oaskew@chicagotechacademy.org>
2015-09-14 14:41:46 -0500

    Create assignment2.rb

---
+++ assignment2.rb
@@ -0,0 +1,3 @@
+Step1: Get2slices of bread and lay it out on a naokin orplate 
+Step2:Get the jelly and peanutbutter & take the top off both and get the knife 
+Step3:Use the knife and pick which ever one you want to start off withe jellly or peanut butter and put one of them on1 sliceof bread Step4:When done putthe 2 slice of bread together &enjoy your sandwich.
